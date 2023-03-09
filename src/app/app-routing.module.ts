1121cf8c2c6e3b40e577a9620e20c63eed21e65a
GabiFerreira-dev <83592846+GabiFerreira-dev@users.noreply.github.com>
2023-03-09 11:42:32 -0300

    Primeiro commit - Modulos e Componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, 
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }, 
+  { path: 'courses', loadChildren: () => import('./courses/courses.module').then(m => m.CoursesModule) }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
